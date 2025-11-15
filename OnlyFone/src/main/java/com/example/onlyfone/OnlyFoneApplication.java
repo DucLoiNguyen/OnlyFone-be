432a4d354491659673ddf9035039c1edfee70889
LoiND <156526656+DucLoiNguyen@users.noreply.github.com>
2025-11-15 23:34:31 +0700

    Initial commit

---
+++ OnlyFone/src/main/java/com/example/onlyfone/OnlyFoneApplication.java
@@ -0,0 +1,13 @@
+package com.example.onlyfone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlyFoneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnlyFoneApplication.class, args);
+    }
+
+}
---
+++ OnlyFone/src/test/java/com/example/onlyfone/OnlyFoneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.onlyfone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlyFoneApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
