fc3b87a6f6e918b27167bd5a50b98c1665672ae9
LoiND <156526656+DucLoiNguyen@users.noreply.github.com>
2025-11-16 22:49:31 +0700

    commit

--- src/main/java/com/example/onlyfone/Controller/TestController.java
+++ src/main/java/com/example/onlyfone/Controller/TestController.java
@@ -7,7 +7,6 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 
 @RestController
 @RequestMapping("/api")
-@CrossOrigin(origins = "http://localhost:5173")
 public class TestController {
 
     @GetMapping("/hello")
